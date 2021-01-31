2fc6468cadfd25445333c2980365c1085d33d1ad
sumit9898 <sumitgeneralmail@gmail.com>
2021-02-01 01:40:08 +0530

    Complete Simon Game project files

---
+++ game.js
@@ -0,0 +1,73 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var started = false;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $('#' + randomChosenColour).fadeTo(100, 0.3, function() {
+    $(this).fadeTo(500, 1.0);
+  });
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function(event) {
+  var userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+
+  }
+  else{
+    var errorSoundName= "wrong";
+    playSound(errorSoundName);
+    $("body").addClass("game-over");
+     setTimeout(function () {
+       $("body").removeClass("game-over");
+     }, 200);
+
+     $("#level-title").text("Game Over, Press Any Key to Restart");
+     $(document).keypress(function startOver(){
+       level = 0;
+       started = false;
+       gamePattern = [];
+     });
+  }
+}
